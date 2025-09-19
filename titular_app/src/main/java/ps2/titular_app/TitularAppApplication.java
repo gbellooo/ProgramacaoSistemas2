a5f1af1ac0e1c196d656c341398cc65dc844ef04
Gabriel Bello <125406298+gbellooo@users.noreply.github.com>
2025-09-19 22:54:25 +0000

    delete

---
+++ titular_app/src/main/java/ps2/titular_app/TitularAppApplication.java
@@ -0,0 +1,13 @@
+package ps2.titular_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitularAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TitularAppApplication.class, args);
+	}
+
+}
